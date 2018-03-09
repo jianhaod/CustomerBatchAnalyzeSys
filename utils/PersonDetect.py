8136261ea12a5a9c0784783365496b26a5cafb10
Jianhao Dai <daipku@163.com>
2018-03-09 17:45:48 +0800

    add thirdparty caffe-fast-rcnn lib support

--- utils/PersonDetect.py
+++ utils/PersonDetect.py
@@ -5,50 +5,56 @@
 Create: 2018/3/8
 @author: Jianhao
 """
+import os, sys
+sys.path.append('../thirdparty/caffe-fast-rcnn/python')
+sys.path.append('../thirdparty/lib')
 
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
 import caffe
 import yaml
 
 class PersonDetector(object):
 
-	def __init__(self, configfile):
-
-		caffeconfig = yaml.load(open(configfile))
-		self.prototxt = caffeconfig['prototxt']
-		self.caffemodel = caffeconfig['caffemodel']
-		self.conf_thresh = caffeconfig['CONF_THRESH']
-		self.nms_thresh = caffeconfig['NMS_THRESH']
-		self.classes = tuple(caffeconfig['netclasses'])
-
-		cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+    def __init__(self, configfile):
+	
+        caffeconfig = yaml.load(open(configfile))
+        self.prototxt = caffeconfig['prototxt']
+        self.caffemodel = caffeconfig['caffemodel']
+        self.conf_thresh = caffeconfig['CONF_THRESH']
+        self.nms_thresh = caffeconfig['NMS_THRESH']
+        self.classes = tuple(caffeconfig['netclasses'])
         
-		caffe.set_mode_gpu()
+        # Use RPN for proposals
+        cfg.TEST.HAS_RPN = True
+        
+        caffe.set_mode_gpu()
         caffe.set_device(0)
-		
+        
         self.net = caffe.Net(self.prototxt, self.caffemodel, caffe.TEST)
 
-	def detect(self, input):
-		
-		image_dic = input[0]
-		local_path = input[1]
-		channel = input[2]
-		
-		local_path_b = local_path.split('/')
-		
-		b_num = len(local_path_b)
-		
-		
-		output_file = config['per_det_output_file']+'/'+local_path_b[b_num - 5]+'/'+local_path_b[b_num - 4]+'/Channel_0'+str(channel)+'/Append'
-		time_file=(local_path.split('/')[-1]).split('.')[0]
-		if not os.path.exists(output_file):
-		    os.makedirs(output_file)
-		
-		file_temp=os.path.join(output_file,time_file+'.txt')
+    def detect(self, input):
+        image_dic = input[0]
+        local_path = input[1]
+        channel = input[2]
+        
+        local_path_b = local_path.split('/')
+        
+        b_num = len(local_path_b)
+        
+        
+        output_file = config['per_det_output_file']+'/'+local_path_b[b_num - 5]+'/'+local_path_b[b_num - 4]+'/Channel_0'+str(channel)+'/Append'
+        time_file=(local_path.split('/')[-1]).split('.')[0]
+        if not os.path.exists(output_file):
+            os.makedirs(output_file)
+        
+        file_temp=os.path.join(output_file,time_file+'.txt')
         #print (file_temp)
-
+        
         f = open(file_temp, 'a')
         for img_num in range(len(image_dic)):
-			im = image_dic[img_num]
+            im = image_dic[img_num]
             #tmpTime = time.time()
             scores, boxes = im_detect(self.net, im)
             #print("imdetect Time: " + str(time.time() - tmpTime))
@@ -82,5 +88,4 @@ class PersonDetector(object):
                     f.write("," + str(cls_ind))
                     # print result
                     f.write('\n')
-
         f.close()
